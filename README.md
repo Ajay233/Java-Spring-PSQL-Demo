305482db9d91b7bb71c64ea42a229b1f56d4612d
Ajay <ajaymungur@hotmail.com>
2019-01-10 16:55:43 +0000

    Code annotated and README updated with instructions

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Demo app using Java, Spring and PostgreSQL
 
+## Dependencies selected on initialisation
+- Web
+- Thymeleaf
+- JDBC
+- PostgreSQL
+
+To setup the database run:
+- ```CREATE DATABASE account```
+- Connect to the ```account``` database (```psql account```)
+- ```CREATE TABLE details ('name' varchar(35), 'account' int, 'pin' int, 'balance' decimal)```
--- src/main/java/bankaccount/demo/Database.java
+++ src/main/java/bankaccount/demo/Database.java
@@ -5,14 +5,15 @@ import java.sql.*;
 public class Database {
 
     private final String url = "jdbc:postgresql://localhost/account";
-    private final String user = "postgres";
-    private final String password = "<add your password>";
+//    private final String user = "postgres";
+//    private final String password = "<add your password>";
 
 
     // Connect to the database and return a connection object
     public Connection conncet(){
         Connection conn = null;
         try{
+            // Establishes the connection and lets the user know connection has been established
             conn = DriverManager.getConnection(url);
             System.out.println("Connection to account established");
         } catch (SQLException e) {
@@ -21,26 +22,58 @@ public class Database {
         } return conn;
     }
 
-    public void addToDb(String name, int accountNumber, int sortcode, int pin, double balance){
+    public void addToDb(String name, int accountNumber, int sortCode, int pin, double balance){
+        // Establishes the connection to the database
         Connection conn = this.conncet();
-        String sql = "INSERT INTO details VALUES ('" + name + "', '" + accountNumber + "', '" + sortcode + "', '" + pin + "', '" + balance + "');";
+        // Creates the SQL query and save it to a string variable
+        String sql = "INSERT INTO details VALUES ('" + name + "', '" + accountNumber + "', '" + sortCode + "', '" + pin + "', '" + balance + "');";
         try {
+            // Created a new statement object to use in carrying out the query
             Statement stmt = conn.createStatement();
+            // Execute the SQL insert statement
             stmt.executeUpdate(sql);
+            //The code below closes the connection, (you wouldn't be able to run another query until this is done)
             stmt.close();
-            conn.commit();
             conn.close();
         } catch (Exception e){
             System.out.println(e.getMessage());
         }
         System.out.println("Record successfully created");
+    }
+
+    public double readBalance(int accountNumber){
+        //Establish the connection to the database
+        Connection conn = this.conncet();
+        //Create the SQL query
+        String sql = "SELECT * FROM details WHERE account = " + accountNumber + ";";
+        //Created a variable below to store the result I'm expecting
+        double balance = 0.0;
+        try {
+            // Created a new statement object to use in carrying out the query
+            Statement stmt = conn.createStatement();
+            // Created a result object for handling the result of the query
+            ResultSet rs = stmt.executeQuery(sql);
+            //The .next() method needs to be called before you can access the result
+            rs.next();
+            balance = rs.getDouble("balance");
+            //The code below closes the connection, (you wouldn't be able to run another query until this is done)
+            stmt.close();
+            conn.close();
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Record successfully created");
+        //Added just to test it's got the balance as the return doesn't print to the screen
+        System.out.println(balance);
+      return balance;
 
     }
 
+    // Created a main method so I could run some commands and see the results
     public static void main(String[] args) {
-        Database start = new Database();
-        start.conncet();
-        start.addToDb("Person", 22334455, 343434, 2345, 23.96);
+        Database db = new Database();
+        db.conncet();
+        db.readBalance(654321);
     }
 
 
